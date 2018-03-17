8132f2870c48387442ed46699b4e70af5c0c7ec4
David Heaps <king.dopey.10111@gmail.com>
2018-03-17 02:28:10 -0700

    Fix setting password in mythweb configuration

--- mythweb/files/dockerentry.sh
+++ mythweb/files/dockerentry.sh
@@ -15,7 +15,7 @@ echo "Setting database configuration"
 sed -i "s/setenv db_server.*/setenv db_server ${DATABASE_HOST}/" /etc/apache2/sites-available/mythweb.conf
 sed -i "s/setenv db_name.*/setenv db_name ${DATABASE_NAME}/" /etc/apache2/sites-available/mythweb.conf
 sed -i "s/setenv db_login.*/setenv db_login ${DATABASE_USER}/" /etc/apache2/sites-available/mythweb.conf
-sed -i "s/setenv db_password.*/setenv db_password ${DATABASE_PASS}/" /etc/apache2/sites-available/mythweb.conf
+sed -i "s/setenv db_password.*/setenv db_password ${DATABASE_PWD}/" /etc/apache2/sites-available/mythweb.conf
 
 echo "starting mythweb"
 /usr/sbin/apache2ctl -D FOREGROUND
