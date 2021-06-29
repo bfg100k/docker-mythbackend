18c8e3608ef5f8ab996cf7dca5a40ab5e18552dd
SidneyC <sidneyc@outlook.com>
2021-06-29 12:14:29 +1000

    merged mythweb into mythbackend

--- mythbackend/files/dockerentry.sh
+++ mythbackend/files/dockerentry.sh
@@ -76,6 +76,12 @@ fi
 
 chown -R mythtv:users /var/lib/mythtv/banners  /var/lib/mythtv/coverart  /var/lib/mythtv/db_backups  /var/lib/mythtv/fanart  /var/lib/mythtv/livetv  /var/lib/mythtv/recordings  /var/lib/mythtv/screenshots  /var/lib/mythtv/streaming  /var/lib/mythtv/trailers  /var/lib/mythtv/videos
 
+echo "Setting database configuration"
+sed -i "s/setenv db_server.*/setenv db_server ${DATABASE_HOST}/" /etc/apache2/sites-available/mythweb.conf
+sed -i "s/setenv db_name.*/setenv db_name ${DATABASE_NAME}/" /etc/apache2/sites-available/mythweb.conf
+sed -i "s/setenv db_login.*/setenv db_login ${DATABASE_USER}/" /etc/apache2/sites-available/mythweb.conf
+sed -i "s/setenv db_password.*/setenv db_password ${DATABASE_PWD}/" /etc/apache2/sites-available/mythweb.conf
+
 #Bring up the backend
 chown -R mythtv:users /var/log/mythtv
 
@@ -85,5 +91,7 @@ do
 echo "waiting....."
 sleep 2s
 done
-echo "start backend"
+echo "starting mythweb"
+/usr/sbin/apache2ctl start
+echo "starting backend"
 exec su - mythtv -c "/usr/bin/mythbackend --syslog local7"
